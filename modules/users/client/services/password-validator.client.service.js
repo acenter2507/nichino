9e955aa5730d51656a5fbb51e632046f96763a2b
acenter2507 <acenter2507@gmail.com>
2018-08-09 17:08:33 +0900

    Config env

--- modules/users/client/services/password-validator.client.service.js
+++ modules/users/client/services/password-validator.client.service.js
@@ -9,8 +9,6 @@
   PasswordValidator.$inject = ['$window'];
 
   function PasswordValidator($window) {
-    var owaspPasswordStrengthTest = $window.owaspPasswordStrengthTest;
-
     var service = {
       getResult: getResult,
       getPopoverMsg: getPopoverMsg
@@ -19,12 +17,11 @@
     return service;
 
     function getResult(password) {
-      var result = owaspPasswordStrengthTest.test(password);
-      return result;
+      return true;
     }
 
     function getPopoverMsg() {
-      var popoverMsg = 'Please enter a passphrase or password with ' + owaspPasswordStrengthTest.configs.minLength + ' or more characters, numbers, lowercase, uppercase, and special characters.';
+      var popoverMsg = 'Message';
 
       return popoverMsg;
     }
