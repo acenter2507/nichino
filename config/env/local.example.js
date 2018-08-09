7e3ceb5cfb6fbf5acb0e43bb133e8c6724395fbb
acenter2507 <acenter2507@gmail.com>
2018-08-09 13:24:57 +0900

    Delete Facebook and Twiter info

--- config/env/local.example.js
+++ config/env/local.example.js
@@ -21,12 +21,6 @@ module.exports = {
   db: {
     uri: 'mongodb://localhost/local-dev',
     options: {}
-  },
-  sessionSecret: process.env.SESSION_SECRET || 'youshouldchangethistosomethingsecret',
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/facebook/callback'
   }
 };
 */
